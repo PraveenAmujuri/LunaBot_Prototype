104ae89def4cecfa32af1af577b7cbbddd7fde44
PraveenAmujuri <saipraveenamujuri@gmail.com>
2025-12-01 10:52:10 +0530

    Update start_project.sh stop_project.sh

--- start_project.sh
+++ start_project.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4b142f231ad627c2596412b696c13bc5a947410cc61502e8d034c4eb05c0c83
-size 24190
+oid sha256:561329653cfda0bce78388a00ce4dc110fca6fe2600a8ac470d96e0b6c29b787
+size 7293
--- stop_project.sh
+++ stop_project.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55276621b0fb89fb3d13888560121475a96bdeafeeea1a1c06369c6cd2365799
-size 15839
+oid sha256:8f24e91334f7c0b351ec4125ed0d8fe2e2cda8e69b3a87b69314c899dd803464
+size 1271
